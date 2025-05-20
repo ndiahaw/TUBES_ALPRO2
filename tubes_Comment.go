cda063a26177c4925fab15e7af9e3680636de777
DhimasNoeryz <dhimasrafinoeryz@gmail.com>
2025-05-21 00:43:38 +0700

    Update tubes_Comment.go
    progress 85%
    code : 90%
    UI : 80%
    
    minus UI aja bang

--- tubes_Comment.go
+++ tubes_Comment.go
@@ -10,8 +10,8 @@ type comment struct {
 }
 type tabComment [NMAX]comment
 
-var kataPositif = []string{"bagus", "indah", "cantik", "ganteng"}
-var kataNegatif = []string{"jelek", "buruk", "menyebalkan", "benci"}
+var komentarPositif = []string{"bagus", "indah", "cantik", "ganteng", "haha", "wih", "keren", "anjay", "bagus", "wow"}
+var komentarNegatif = []string{"jelek", "buruk", "menyebalkan", "benci", "burik", "gaje", "anjir", "gelo", "shibal", "najong"}
 var tab tabComment
 var IDKomen int
 var nData int = 1
@@ -41,7 +41,7 @@ func inputComment(A *tabComment, nData *int) {
 	}
 	fmt.Println("")
 	fmt.Println("Komentar berhasil ditambahkan!")
-	fmt.Println("Komentar yang di masukan :", *nData)
+	fmt.Println("Komentar yang di masukan :", *nData-1)
 	outputComment(*A, *nData)
 }
 
@@ -139,13 +139,13 @@ func analisisSentimen(A *tabComment, nData int) {
 	for i < nData {
 		x = false
 		y = false
-		for j = 0; j < len(kataPositif); j++ {
-			if cariKataKunci((*A)[i], kataPositif[j]) {
+		for j = 0; j < len(komentarPositif); j++ {
+			if cariKataKunci((*A)[i], komentarPositif[j]) {
 				x = true
 			}
 		}
-		for j = 0; j < len(kataNegatif); j++ {
-			if cariKataKunci((*A)[i], kataNegatif[j]) {
+		for j = 0; j < len(komentarNegatif); j++ {
+			if cariKataKunci((*A)[i], komentarNegatif[j]) {
 				y = true
 			}
 		}
@@ -214,24 +214,49 @@ func cariKataKunci(A comment, kataKunci string) bool {
 }
 
 // fungsi ini buat ngurutin komentar berdasarkan panjang teksnya menggunakan selection sort
-func urutkanPanjangKomentar(A *tabComment, nData int) {
+func urutkanPanjangKomentar(A *tabComment, nData, pilihan int) {
 	var i, idx, pass int
 	var temp comment
 	pass = 2
-	for pass < nData {
-		idx = pass - 1
-		i = pass
-		temp = A[pass]
-		for i < nData {
-			if A[i].longcomment > A[idx].longcomment {
-				idx = i
+
+	if pilihan == 1 {
+		fmt.Println("Komentar Ascending sebelum diurutkan :")
+		outputComment(*A, nData)
+		for pass < nData {
+			idx = pass - 1
+			i = pass
+			temp = A[pass]
+			for i < nData {
+				if A[i].longcomment < A[idx].longcomment {
+					idx = i
+				}
+				i++
 			}
-			i++
+			temp = A[pass-1]
+			A[pass-1] = A[idx]
+			A[idx] = temp
+			pass++
 		}
-		temp = A[pass-1]
-		A[pass-1] = A[idx]
-		A[idx] = temp
-		pass++
+		fmt.Println("Komentar Ascending setelah diurutkan : ")
+	} else if pilihan == 2 {
+		fmt.Println("Komentar Descending sebelum diurutkan :")
+		outputComment(*A, nData)
+		for pass < nData {
+			idx = pass - 1
+			i = pass
+			temp = A[pass]
+			for i < nData {
+				if A[i].longcomment > A[idx].longcomment {
+					idx = i
+				}
+				i++
+			}
+			temp = A[pass-1]
+			A[pass-1] = A[idx]
+			A[idx] = temp
+			pass++
+		}
+		fmt.Println("Komentar Desscending setelah diurutkan : ")
 	}
 }
 
@@ -243,7 +268,7 @@ func urutkanSentimenKomentar(A *tabComment, nData int) {
 
 	analisisSentimen(*&A, nData)
 
-	fmt.Println("Komentar sebelum diurutkan :")
+	fmt.Println("Komentar Sentimen sebelum diurutkan :")
 	outputComment(*A, nData)
 
 	for pass < nData {
@@ -256,14 +281,13 @@ func urutkanSentimenKomentar(A *tabComment, nData int) {
 		A[i] = temp
 		pass++
 	}
-	fmt.Println("Komentar setelah diurutkan :")
-	outputComment(*A, nData)
+	fmt.Println("Komentar Sentimen setelah diurutkan :")
 }
 
 func urutkanKomentar(A *tabComment, nData int) {
 	// buat array comment
 	var pilihan int
-	urutkanPanjangKomentar(&tab, nData)
+	urutkanPanjangKomentar(&tab, nData, pilihan)
 	fmt.Println("")
 	fmt.Println("1. Berdasarkan Panjang Komentar")
 	fmt.Println("2. Berdasarkan Sentimen Komentar")
@@ -271,28 +295,33 @@ func urutkanKomentar(A *tabComment, nData int) {
 	fmt.Println("Silahkan Pilih Tipe Pengurutan : ")
 	fmt.Scan(&pilihan)
 	if pilihan == 1 {
-		urutkanPanjangKomentar(&tab, nData)
+		fmt.Println("1. Ascending")
+		fmt.Println("2. Descending")
+		fmt.Print("Pilih urutan: ")
+		fmt.Scan(&pilihan)
+		urutkanPanjangKomentar(A, nData, pilihan)
 	} else if pilihan == 2 {
-		urutkanSentimenKomentar(&tab, nData)
+		urutkanSentimenKomentar(A, nData)
 	}
+	outputComment(*A, nData)
 }
 
 func statistikSentimen(A *tabComment, nData int) {
 	var positif, negatif, netral, i int
-	for i = 0; i < nData; i++ {
-		if A[i].sentimen == "positif" {
+	for i = 1; i < nData; i++ {
+		if A[i].sentimen == "Positif" {
 			positif = positif + 1
-		} else if A[i].sentimen == "negatif" {
+		} else if A[i].sentimen == "Negatif" {
 			negatif = negatif + 1
-		} else if A[i].sentimen == "netral" {
+		} else if A[i].sentimen == "Netral" {
 			netral = netral + 1
 		}
 	}
 	fmt.Println()
 	fmt.Println("Statistik Sentimen Komentar : ")
 	fmt.Println("Positif:", positif, "komentar")
-	fmt.Println("Negatif:", negatif, "komentar")
 	fmt.Println("Netral :", netral, "komentar")
+	fmt.Println("Negatif:", negatif, "komentar")
 	outputComment(tab, nData)
 
 }
